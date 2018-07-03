ba8a98cb1ef158f482f94c672d91aa813a5ba911
Volodymyr <34250821+VoBilyk@users.noreply.github.com>
2018-07-04 00:17:30 +0300

    added requests

--- Lesson1_StructuresAndLINQ/WebRequest.cs
+++ Lesson1_StructuresAndLINQ/WebRequest.cs
@@ -12,9 +12,9 @@ using Lesson1_StructuresAndLINQ.Model;
 
 namespace Lesson1_StructuresAndLINQ
 {
-    public class WebRequest
+    public class DB
     {
-        public static string CreateURL(string endpoint)
+        public string CreateURL(string endpoint)
         {
             var possibleEndpoints = new string[] { "users", "posts", "comments", "todos", "address" };
             if (!possibleEndpoints.Contains(endpoint))
@@ -26,16 +26,54 @@ namespace Lesson1_StructuresAndLINQ
         }
 
 
-        public static IEnumerable<User> GetUsers(string url)
+        public List<User> CreateDB()
         {
-            List<User> users = null;
-            var client = new HttpClient();
+            var url = @"https://5b128555d50a5c0014ef1204.mockapi.io/";
 
-            var userResponse = client.GetAsync(url).Result;
-            if (userResponse.IsSuccessStatusCode)
+            List<User> users = null;
+            List<Post> posts = null;
+            List<Todo> todos = null;
+            List<Comment> comments = null;
+
+            var client = new HttpClient();
+            
+            var usersResponse = client.GetAsync(url + "users").Result;
+            if (usersResponse.IsSuccessStatusCode)
             {
-                string userJSON = userResponse.Content.ReadAsStringAsync().Result;
+                string userJSON = usersResponse.Content.ReadAsStringAsync().Result;
                 users = JsonConvert.DeserializeObject<List<User>>(userJSON);
+            }
+            
+            var postsResponse = client.GetAsync(url + "posts").Result;
+            if (postsResponse.IsSuccessStatusCode)
+            {
+                string postsJSON = postsResponse.Content.ReadAsStringAsync().Result;
+                posts = JsonConvert.DeserializeObject<List<Post>>(postsJSON);
+            }
+
+            var todosResponse = client.GetAsync(url + "todos").Result;
+            if (todosResponse.IsSuccessStatusCode)
+            {
+                string todosJSON = todosResponse.Content.ReadAsStringAsync().Result;
+                todos = JsonConvert.DeserializeObject<List<Todo>>(todosJSON);
+            }
+
+            var commentsResponse = client.GetAsync(url + "comments").Result;
+            if (commentsResponse.IsSuccessStatusCode)
+            {
+                string commentsJSON = commentsResponse.Content.ReadAsStringAsync().Result;
+                comments = JsonConvert.DeserializeObject<List<Comment>>(commentsJSON);
+            }
+
+            foreach (var post in posts)
+            {
+                post.Comments = comments.Where(x => x.PostId == post.Id).ToList();
+            }
+
+            foreach (var user in users)
+            {
+                user.Todos = todos.Where(x => x.UserId == user.Id).ToList();
+                user.Posts = posts.Where(x => x.UserId == user.Id).ToList();
             }
 
             return users;
